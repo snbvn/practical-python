6de59746a65c85cb209a061a99075f3d37c52cf1
Sanaz <snbvn@DESKTOP-72U5J5B.localdomain>
2020-07-09 16:02:20 -0230

    Assignment4

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,24 @@
 # pcost.py
 #
 # Exercise 1.27
+import sys
+def portfolio_cost(filename): 
+    price=0
+    f = open(filename, 'rt')
+    headers = next(f).split(',')
+    
+    for line in f:
+        row = line.split(',')
+        try:       
+            price= price + int(row[1])* float(row[2])
+        except ValueError:
+            print("error")
+    return price
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
