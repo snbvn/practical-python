c51dee40a196ba601d8dfcf8d355a24760f71434
sanaz.nabavian <snabavian@mun.ca>
2020-07-16 01:26:26 -0230

    Exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,13 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+portfolio=[]
+def read_portfolio(filename):
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            #row = line.split(',')
+            portfolio.append ((row[0],int(row[1]),float(row[2])))
+    return portfolio
