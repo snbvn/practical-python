5297a3cede7be8c6abddf87ceea1ec791fec0f43
sanaz.nabavian <snabavian@mun.ca>
2020-07-17 23:39:13 -0230

    Exercise 2.16

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,24 +1,37 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
 import sys
+
 def portfolio_cost(filename): 
+    total_cost = 0.0
     price=0
     f = open(filename, 'rt')
-    headers = next(f).split(',')
+    rows = csv.reader(f)
+    headers = next(rows)
     
-    for line in f:
-        row = line.split(',')
-        try:       
-            price= price + int(row[1])* float(row[2])
-        except ValueError:
-            print("error")
-    return price
-
+    
+    #    try:       
+    #        price= price + int(row[1])* float(row[2])
+    #    except ValueError:
+    #        print("error")
+    #return price
+    for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
+            try:
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                total_cost += nshares * price
+                
+            # This catches errors in int() and float() conversions above
+            except ValueError:
+                print(f'Row {rowno}: Bad row: {row}')
+    return total_cost
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 else:
-    filename = 'Data/portfolio.csv'
+    filename = 'Data/portfoliodate.csv'
 
 cost = portfolio_cost(filename)
 print('Total cost:', cost)
--- Work/report.py
+++ Work/report.py
@@ -8,11 +8,12 @@ def read_portfolio(filename):
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
+            record = dict(zip(headers, row))
             stock = {
-                'name'   : row[0],
-                'shares' : int(row[1]),
-                'price'   : float(row[2])
-                }
+                 'name'   : record['name'],
+                 'shares' : int(record['shares']),
+                 'price'   : float(record['price'])
+            }
             portfolio.append(stock)
     return portfolio
 
